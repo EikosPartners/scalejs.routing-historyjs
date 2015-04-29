d9777d8cc2da2f1795530427efc7aff1384d5988
Peter Fatyga <peter.fatyga@eikospartners.com>
2015-04-29 11:32:04 -0400

    Initial commit

---
+++ src/scalejs.routing-historyjs.js
@@ -0,0 +1,12 @@
+/*global define*/
+define([
+    'scalejs!core',
+    './scalejs.routing-historyjs/routing'
+], function (
+    core,
+    routing
+) {
+    'use strict';
+
+    core.registerExtension({ routing: routing });
+});
